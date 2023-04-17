9920b119721f9d3e50bf8e29300383f5f5754890
Marçal <byelmarcal@hotmail.com>
2023-04-17 10:49:36 -0300

    h2 Database configuration

--- src/main/java/com/marcaly/springcourse/entities/User.java
+++ src/main/java/com/marcaly/springcourse/entities/User.java
@@ -1,7 +1,9 @@
 package com.marcaly.springcourse.entities;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Generated;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
@@ -9,9 +11,13 @@ import java.io.Serializable;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
