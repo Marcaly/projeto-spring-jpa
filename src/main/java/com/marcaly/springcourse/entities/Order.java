22b1c131f862c26a23627e4380282ee021316764
Marçal <byelmarcal@hotmail.com>
2023-04-17 19:10:43 -0300

    enum orderstatus

--- src/main/java/com/marcaly/springcourse/entities/Order.java
+++ src/main/java/com/marcaly/springcourse/entities/Order.java
@@ -1,14 +1,16 @@
 package com.marcaly.springcourse.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.marcaly.springcourse.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.time.Instant;
-@Data
+@EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
@@ -18,10 +20,52 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+        setOrderStatus(orderStatus);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf( orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null)
+        this.orderStatus = orderStatus.getCode();
+    }
 }
