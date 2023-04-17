9bd8a48ac133212136e3d532f0855071039fc7c0
Marçal <byelmarcal@hotmail.com>
2023-04-17 18:28:27 -0300

    Adicionei a order entity

---
+++ src/main/java/com/marcaly/springcourse/entities/Order.java
@@ -0,0 +1,25 @@
+package com.marcaly.springcourse.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.Instant;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+}
